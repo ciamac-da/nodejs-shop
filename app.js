5c0f88b1dc06af0fef1ce8fddd710dae20e85e60
ciamac-da <ciamac.davoudi@gmail.com>
2021-05-06 19:50:00 +0200

    Verified by Marti4n v1.2 mongoose

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const mongoose = require("mongoose")
-/* const rootDir = require("./util/path"); */
+const User = require('./models/user');
+
 
 const app = express();
 const PORT = process.env.PORT || 5005;
@@ -19,6 +20,17 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use((req, res, next) => {
+    User.findById('5bab316ce0a7c75f783cb8a8')
+      .then(user => {
+        req.user = user;
+        next();
+      })
+      .catch(err => console.log(err));
+  });
+
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -31,12 +43,23 @@ mongoose.connect(
     { useNewUrlParser: true },
     { useUnifiedTopology: true }
     )
-    .then(result => {
-        app.listen(PORT);
-    })
-    .catch(err => {
-        console.log(err)
-    })
-
+     .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'CiHakT0r',
+          email: 'ciamax@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    app.listen(PORT);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 
