e4e00e27eaca97c7012c8f67d2ee18c9b0a64fa2
ciamac-da <ciamac.davoudi@gmail.com>
2021-05-09 15:08:02 +0200

    Verified by Marti4n v1.2.3 Authentication finished

--- controllers/auth.js
+++ controllers/auth.js
@@ -169,3 +169,51 @@ crypto.randomBytes(32, (err, buffer) => {
   .catch(err => console.log(err))
 });
 };
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  User.findOne({resetToken:token, resetTokenExpiration: {$gt: Date.now()}})
+  .then( user => {
+    let message = req.flash('error');
+    if (message.length > 0) {
+      message = message[0];
+    } else {
+      message = null;
+    }
+    res.render('auth/new-password', {
+      path: '/new-password',
+      pageTitle: 'New Password',
+      errorMessage: message,
+      userId: user._id.toString(),
+      passwordToken:token
+    });
+  })
+    .catch(err=> console.log(err))
+  }
+
+  exports.postNewPassword = (req, res, next) => {
+    const newPassword = req.body.password;
+    const userId = req.body.userId;
+    const passwordToken = req.body.passwordToken;
+    let resetToken;
+
+    User.findOne({
+      resetToken: passwordToken,
+      resetTokenExpiration: {$gt: Date.now()},
+      _id:userId
+    })
+    .then(user => {
+      resetToken = user;
+      return bcrypt.hash(newPassword, 12);
+    })
+    .then(hashedPassword => {
+      resetUser.password = hashedPassword;
+      resetUser.resetToken = undefined;
+      resetUser.resetTokenExpiration = undefined;
+      resetUser.save()
+    })
+    .then(result => {
+      res.redirect("/login");
+    })
+    .catch(err => console.log(err))
+  }
