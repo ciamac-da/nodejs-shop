0eba214aabcca797ac5cd3326eb1b0474c3779cd
ciamac-da <ciamac.davoudi@gmail.com>
2021-05-09 18:21:13 +0200

    Verified by Marti4n v1.2.4 validation started & email & password errors implemented, match passwords done

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const crypto = require("crypto");
+const { validationResult } = require("express-validator/check");
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
@@ -77,6 +78,14 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+  if(!errors.isEmpty()) {
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg
+    })
+  }
   User.findOne({ email: email })
     .then(userDoc => {
       if (userDoc) {
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const { check, body } = require("express-validator/check")
 const authController = require('../controllers/auth');
 
 const router = express.Router();
@@ -10,7 +10,27 @@ router.get('/signup', authController.getSignup);
 
 router.post('/login', authController.postLogin);
 
-router.post('/signup', authController.postSignup);
+router.post('/signup',
+[
+check("email")
+.isEmail()
+.withMessage("Please add a valid email!")
+.custom((value, {req}) => {
+    if(value === "test@test.com") {
+        throw new Error("This email address is forbidden")
+    }
+    return true;
+}),
+//body("password").isLength({min:12}).withMessage("Please enter at least 12 characters!")
+body("password", "Please enter at least 12 characters!").isLength({min:12}), // To avoid repeating message
+body("confirmPassword").custom((value, {req}) => {
+    if(value !== req.body.password) {
+        throw new Error("Passwords have to match!")
+    }
+    return true
+})
+], 
+authController.postSignup);
 
 router.post('/logout', authController.postLogout);
 
